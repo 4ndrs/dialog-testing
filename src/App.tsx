da382b086c234a24f0673e26574838428b40535f
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-05-06 13:28:02 +0200

    Handle backdrop click

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,26 @@ const App = () => {
     dialogRef.current.showModal();
   };
 
+  const handleBackdropClick = (event: React.MouseEvent) => {
+    if (!dialogRef.current) {
+      return;
+    }
+
+    if (event.target === event.currentTarget) {
+      dialogRef.current.close();
+    }
+  };
+
   return (
     <>
       <button onClick={handleClick}>Open Dialog</button>
 
-      <dialog ref={dialogRef}>
-        <form method="dialog">
+      <dialog
+        ref={dialogRef}
+        onClick={handleBackdropClick}
+        style={{ padding: 0 }}
+      >
+        <form method="dialog" style={{ padding: 10 }}>
           <p>Hi, I'm a dialog.</p>
           <button>OK</button>
         </form>
