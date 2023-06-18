f837cea5cfc186ad1483560ba45d6a4b7f1875ce
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-18 18:13:42 +0200

    Add dialog 2 test

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { useRef } from "react";
 
 const App = () => {
   const dialogRef = useRef<HTMLDialogElement>(null);
+  const dialogRef2 = useRef<HTMLDialogElement>(null);
 
   const handleClick = () => {
     dialogRef.current?.showModal();
+  };
+
+  const handleClick2 = () => {
+    dialogRef2.current?.show();
   };
 
   const handleBackdropClick = (event: React.MouseEvent) => {
@@ -27,6 +32,30 @@ const App = () => {
           <button>OK</button>
         </form>
       </dialog>
+
+      <div
+        style={{
+          position: "absolute",
+          bottom: "15px",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        <button onClick={handleClick2}>Open Dialog 2</button>
+        <dialog
+          ref={dialogRef2}
+          style={{
+            position: "absolute",
+            top: "-10px",
+            transform: "translateY(-100%)",
+          }}
+        >
+          <form method="dialog" style={{ padding: 10 }}>
+            <p>Hi, I'm a dialog 2!</p>
+            <button>OK</button>
+          </form>
+        </dialog>
+      </div>
     </>
   );
 };
